a8b721373d599a049c4125ad9b8a260d54dc41b9
Mateo Del Rosario Chauchet <mateo.chauchet@mercadolibre.com>
2024-11-10 10:38:53 -0300

    initial page structure

---
+++ src/app/mobile/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const MobileHome = () => {
+  return (
+    <div>
+      <h2>home</h2>
+      <Link href="mobile/professionals">Profesionals</Link>
+    </div>
+  );
+};
+
+export default MobileHome;
---
+++ src/app/mobile/professionals/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import React from "react";
+
+const ProfessionalsPage = () => {
+  return (
+    <div>
+      <h2>professionalsPage</h2>
+      <Link href="professionals/5">Profesional Detail</Link>
+    </div>
+  );
+};
+
+export default ProfessionalsPage;
