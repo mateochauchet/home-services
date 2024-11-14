22e3f4ebc181ebe5465e2ef2a9d79a9dde15f760
mateochauchet <chauchetmateo@gmail.com>
2024-11-14 20:30:26 -0300

    V1

--- src/backend/repositories/professional.repository.ts
+++ src/backend/repositories/professional.repository.ts
@@ -1,12 +1,24 @@
 import { prisma } from "@/backend/db/prisma";
 import { Professional } from "@prisma/client";
 
+interface ProfessionalFilters {
+  location?: string;
+  serviceId?: number;
+  minRating?: number;
+}
+
 export class ProfessionalRepository {
-  async findAll(): Promise<Professional[]> {
+  async findAll(filters: ProfessionalFilters = {}): Promise<Professional[]> {
+    const { location, serviceId, minRating } = filters;
     try {
       return prisma.professional.findMany({
         include: {
           user: true,
+        },
+        where: {
+          ...(location && { location }),
+          ...(minRating && { rating: { gte: minRating } }),
+          ...(serviceId && { services: { some: { serviceId } } }),
         },
       });
     } catch (error) {
